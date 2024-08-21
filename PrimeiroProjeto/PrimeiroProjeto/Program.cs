5502ffab19097914638de56db0912cc4cc8912e7
Bryan Fusco <bryanratin8@gmail.com>
2024-08-21 09:16:15 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("qual teu nome patrao");
+        string nome = Console.ReadLine();
+        Console.WriteLine("seu nome é " + nome);
+    }
+}
