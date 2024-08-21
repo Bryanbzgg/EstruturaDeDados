e4e5e5a363fb6140efd04abb06fe30e1fc180519
Bryan Fusco <bryanratin8@gmail.com>
2024-08-21 10:38:41 -0300

    Trabalhando com Branchs, teste 2.

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -3,6 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Minha primeira branch");
+        Console.WriteLine("trabalhando com Git/Github");
+        Console.WriteLine("Mia uma tentativa de traalhar com Git");
 
     }
 }
